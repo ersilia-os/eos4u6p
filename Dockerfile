4d2fd0ad232bb1b321b387cdd53996340a1ac8df
miquelduranfrigola <miquelduranfrigola@gmail.com>
2021-10-04 10:47:08 +0200

    refactored to work on pre-downloaded checkpoints

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM bentoml/model-server:0.11.0-py37
 MAINTAINER ersilia
 
-RUN pip install rdkit-pypi
+RUN conda install -c conda-forge rdkit=2019.03.01
 RUN pip install signaturizer==1.1.10
 
 WORKDIR /repo
--- src/service.py
+++ src/service.py
@@ -1,61 +1,93 @@
-from signaturizer import Signaturizer
-import pickle
-import os
-
+import json
 from typing import List
+
 from bentoml import BentoService, api, artifacts
 from bentoml.adapters import JsonInput
 from bentoml.types import JsonSerializable
 from bentoml.service import BentoServiceArtifact
 
-DATASETS = ["{0}{1}".format(x, y) for x in "ABCDE" for y in "12345"]
-
-VERSION = "2020_02"
+import pickle
+import os
+import shutil
+import collections
+import tempfile
+import subprocess
+import csv
 
 CHECKPOINTS_BASEDIR = "checkpoints"
-FRAMEWORK_BASEDIR = None
+FRAMEWORK_BASEDIR = "framework"
 
-
-def load_model():
-    mdl = SignaturizerModel()
-    mdl.load(CHECKPOINTS_BASEDIR, FRAMEWORK_BASEDIR)
+def load_model(framework_dir, checkpoints_dir):
+    mdl = Model()
+    mdl.load(framework_dir, checkpoints_dir)
     return mdl
 
 
-class SignaturizerModel(object):
+class Model(object):
     def __init__(self):
-        pass
+        self.DATA_FILE = "data.csv"
+        self.PRED_FILE = "pred.json"
+        self.RUN_FILE = "run.sh"
+        self.LOG_FILE = "run.log"
 
     def load(self, framework_dir, checkpoints_dir):
         self.framework_dir = framework_dir
         self.checkpoints_dir = checkpoints_dir
 
-    def _get_model_path(self, dataset):
-        return os.path.join(self.checkpoints_dir, "{0}".format(dataset))
+    def set_checkpoints_dir(self, dest):
+        self.checkpoints_dir = os.path.abspath(dest)
+
+    def set_framework_dir(self, dest):
+        self.framework_dir = os.path.abspath(dest)
 
     def predict(self, smiles_list):
-        res_ = {}
-        for ds in DATASETS:
-            path = self._get_model_path(ds)
-            sign = Signaturizer(model_name=path, local=True)
-            X = sign.predict(smiles_list).signature
-            res_[ds] = X
-        result = []
-        for i in range(len(smiles_list)):
-            d = {}
-            for k, v in res_.items():
-                d[k] = list(v[i])
-            result += [d]
+        tmp_folder = tempfile.mkdtemp()
+        data_file = os.path.join(tmp_folder, self.DATA_FILE)
+        pred_file = os.path.join(tmp_folder, self.PRED_FILE)
+        log_file = os.path.join(tmp_folder, self.LOG_FILE)
+        with open(data_file, "w") as f:
+            f.write("smiles"+os.linesep)
+            for smiles in smiles_list:
+                f.write(smiles + os.linesep)
+        run_file = os.path.join(tmp_folder, self.RUN_FILE)
+        with open(run_file, "w") as f:
+            lines = [
+                "python {0}/predict.py {1} {2}".format(
+                    self.framework_dir,
+                    data_file,
+                    pred_file
+                )
+            ]
+            f.write(os.linesep.join(lines))
+        cmd = "bash {0}".format(run_file)
+        with open(log_file, "w") as fp:
+            subprocess.Popen(
+                cmd, stdout=fp, stderr=fp, shell=True, env=os.environ
+            ).wait()
+        with open(pred_file, "r") as f:
+            result = json.load(f)
         return result
 
 
-class SignaturizerArtifact(BentoServiceArtifact):
-    """Dummy artifact"""
-
+class Artifact(BentoServiceArtifact):
     def __init__(self, name):
-        super(SignaturizerArtifact, self).__init__(name)
+        super(Artifact, self).__init__(name)
         self._model = None
         self._extension = ".pkl"
+
+    def _copy_checkpoints(self, base_path):
+        src_folder = self._model.checkpoints_dir
+        dst_folder = os.path.join(base_path, "checkpoints")
+        if os.path.exists(dst_folder):
+            os.rmdir(dst_folder)
+        shutil.copytree(src_folder, dst_folder)
+
+    def _copy_framework(self, base_path):
+        src_folder = self._model.framework_dir
+        dst_folder = os.path.join(base_path, "framework")
+        if os.path.exists(dst_folder):
+            os.rmdir(dst_folder)
+        shutil.copytree(src_folder, dst_folder)
 
     def _model_file_path(self, base_path):
         return os.path.join(base_path, self.name + self._extension)
@@ -67,16 +99,24 @@ class SignaturizerArtifact(BentoServiceArtifact):
     def load(self, path):
         model_file_path = self._model_file_path(path)
         model = pickle.load(open(model_file_path, "rb"))
+        model.set_checkpoints_dir(
+            os.path.join(os.path.dirname(model_file_path), "checkpoints")
+        )
+        model.set_framework_dir(
+            os.path.join(os.path.dirname(model_file_path), "framework")
+        )
         return self.pack(model)
 
     def get(self):
         return self._model
 
     def save(self, dst):
+        self._copy_checkpoints(dst)
+        self._copy_framework(dst)
         pickle.dump(self._model, open(self._model_file_path(dst), "wb"))
 
 
-@artifacts([SignaturizerArtifact("model")])
+@artifacts([Artifact("model")])
 class Service(BentoService):
     @api(input=JsonInput(), batch=True)
     def predict(self, input: List[JsonSerializable]):
