877ba209836eb7e1ef16753063e9fc217c4800db
GemmaTuron <gemma@ersilia.io>
2025-12-24 10:47:50 +0100

    add ersilia-pack-utils

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN conda install -c conda-forge hdf5=1.14.6
 RUN pip install rdkit-pypi==2022.9.5
 RUN pip install signaturizer==1.1.10
 RUN pip install typing-extensions==4.12.2
+RUN pip install ersilia-pack-utils==0.1.5
 
 WORKDIR /repo
 COPY . /repo
--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,6 +1,7 @@
-import csv, os, struct, sys, json
+import os, sys
 from signaturizer import Signaturizer
 import numpy as np
+from ersilia_pack_utils.core import read_smiles, write_out
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
@@ -12,78 +13,8 @@ outfile = sys.argv[2]
 ROOT = os.path.abspath(os.path.dirname(__file__))
 checkpoints_dir = os.path.join(ROOT, "..", "..", "checkpoints")
 
-
-def read_smiles_csv(in_file):
-  with open(in_file, "r") as f:
-    reader = csv.reader(f)
-    cols = next(reader)
-    data = [r[0] for r in reader]
-    return cols, data
-
-
-def read_smiles_bin(in_file):
-  with open(in_file, "rb") as f:
-    data = f.read()
-
-  mv = memoryview(data)
-  nl = mv.tobytes().find(b"\n")
-  meta = json.loads(mv[:nl].tobytes().decode("utf-8"))
-  cols = meta.get("columns", [])
-  count = meta.get("count", 0)
-
-  smiles_list = [None] * count
-  offset = nl + 1
-  for i in range(count):
-    (length,) = struct.unpack_from(">I", mv, offset)
-    offset += 4
-    smiles_list[i] = mv[offset : offset + length].tobytes().decode("utf-8")
-    offset += length
-
-  return cols, smiles_list
-
-
-def read_smiles(in_file):
-  if in_file.endswith(".bin"):
-    return read_smiles_bin(in_file)
-  return read_smiles_csv(in_file)
-
-
-def write_out_csv(results, header, file):
-  with open(file, "w") as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
-    for r in results:
-      writer.writerow(r)
-
-
-def write_out_bin(results, header, file):
-  arr = np.asarray(results, dtype=np.float32)
-  meta = {"columns": header, "shape": arr.shape, "dtype": "float32"}
-  meta_bytes = (json.dumps(meta) + "\n").encode("utf-8")
-
-  with open(file, "wb") as f:
-    f.write(meta_bytes)
-    f.truncate(len(meta_bytes) + arr.nbytes)
-
-  m = np.memmap(
-    file, dtype=arr.dtype, mode="r+", offset=len(meta_bytes), shape=arr.shape
-  )
-  m[:] = arr
-  m.flush()
-
-
-def write_out(results, header, file):
-  if file.endswith(".bin"):
-    write_out_bin(results, header, file)
-  elif file.endswith(".csv"):
-    write_out_csv(results, header, file)
-  else:
-    raise ValueError(f"Unsupported extension for {file!r}")
-
-
 def get_model_path(ds):
   return os.path.join(checkpoints_dir, ds)
-
 
 def predict_signatures(smiles_list):
   res_ = {}
@@ -100,7 +31,6 @@ def predict_signatures(smiles_list):
     result += [d]
   output = {"result": result, "meta": None}
   return output
-
 
 def refactor(output_dict):
   data = output_dict["result"]
@@ -120,9 +50,8 @@ for x in "abcde":
     for i in range(128):
       header += ["{0}{1}_{2}".format(x, y, str(i).zfill(3))]
 
-smiles = read_smiles(infile)[1]
+_,smiles = read_smiles(infile)[1]
 data = predict_signatures(smiles)
 outputs = refactor(data)
 
-
-write_out(outputs, header, outfile)
+write_out(outputs, header, outfile, np.float32)
